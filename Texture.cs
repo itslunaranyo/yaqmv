57d4e7f864adb5208b75b3546b530d43c7d864d0
Matthew Breit <itslunaranyo@gmail.com>
2024-05-15 11:38:49 -0700

    remove test junk

--- Texture.cs
+++ Texture.cs
@@ -27,25 +27,17 @@ namespace yaqmv
 		{
 			Handle = GL.GenTexture();
 
-			byte[] fakedata = {
-				128, 0, 255, 255, 128, 128, 0, 0, 128, 0, 0, 0, 128, 128, 0, 0,
-				128, 128, 0, 0, 128, 0, 0, 0, 128, 128, 0, 0, 128, 0, 0, 0,
-				128, 0, 0, 0, 128, 128, 0, 0, 128, 0, 0, 0, 128, 128, 0, 0,
-				128, 128, 0, 0, 128, 0, 0, 0, 128, 128, 0, 0, 128, 0, 0, 0,
-			};
-
 			GL.BindTexture(TextureTarget.Texture2D, Handle);
 			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
 			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest); 
 			GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, w, h, 0, PixelFormat.Rgba, PixelType.UnsignedByte, pixeldata);
-			//GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, 4, 4, 0, PixelFormat.Rgba, PixelType.UnsignedByte, fakedata);
-			GLWhatsWrong();
+			//GLWhatsWrong();
 		}
 		public void Bind(TextureUnit unit = TextureUnit.Texture0)
 		{
 			GL.ActiveTexture(unit);
 			GL.BindTexture(TextureTarget.Texture2D, Handle);
-			GLWhatsWrong();
+			//GLWhatsWrong();
 		}
 	}
 }
