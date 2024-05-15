b61409e85bb00d16bd49f134bc7432c72d0230f2
Matthew Breit <itslunaranyo@gmail.com>
2024-05-14 17:05:02 -0700

    add easy resource reading method

---
+++ Utility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Resources;
+
+namespace yaqmv
+{
+	public partial class Resource
+	{
+		public static string ReadText(string path)
+		{
+			StreamResourceInfo srinfo = YAQMVApp.GetResourceStream(new Uri(path, UriKind.Relative));
+			StreamReader sr = new StreamReader(srinfo.Stream);
+			string read = sr.ReadToEnd();
+			return read;
+		}
+	}
+
+}
